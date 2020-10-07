a70fb7eebeb56bfa82e9ce62d613c6b93acb1055
Ana Clara Romeiro <clararomeirocosta@gmail.com>
2020-10-07 15:56:58 -0300

    Enum Sanduiches
    Criação da enumeração de sanduiches.

---
+++ src/Sanduiches.java
@@ -0,0 +1,9 @@
+public enum Sanduiches {
+    BBH(6.14), NCT(5.00), ZENDAYA(11.50), TWICE(14.50), Priscila_Senna(7.00);
+
+    double valor;
+
+    Sanduiches(double n){
+        valor = n;
+    }
+}
