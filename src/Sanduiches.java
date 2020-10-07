8c4b55b28de3fb415683a983a023bc2853a81060
Ana Clara Romeiro <clararomeirocosta@gmail.com>
2020-10-07 16:00:31 -0300

    Revert "Enum Sanduiches"
    This reverts commit a70fb7eebeb56bfa82e9ce62d613c6b93acb1055.

--- src/Sanduiches.java
+++
@@ -1,9 +0,0 @@
-public enum Sanduiches {
-    BBH(6.14), NCT(5.00), ZENDAYA(11.50), TWICE(14.50), Priscila_Senna(7.00);
-
-    double valor;
-
-    Sanduiches(double n){
-        valor = n;
-    }
-}
