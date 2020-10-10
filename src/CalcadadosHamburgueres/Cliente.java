70a312b67c6061aaeed8293d25f53fdd9d90a05a
Aldenir <aldenirtelless@gmail.com>
2020-10-09 23:49:23 -0300

    codigo

--- src/CalcadadosHamburgueres/Cliente.java
+++ src/CalcadadosHamburgueres/Cliente.java
@@ -1,5 +1,51 @@
 package CalcadadosHamburgueres;
 
 public class Cliente {
+	
+	private String nome;
+	private Long cpf;
+	
+	public Cliente(String nome, Long cpf)
+	{
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	
+	public String getNome() 
+	{
+		return nome;
+	}
+
+	public void setNome(String nome) 
+	{
+		this.nome = nome;
+	}
+
+	public Long getCpf() 
+	{
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) 
+	{
+		this.cpf = cpf;
+	}
+	
+
+	public void verCardapio()
+	{
+		Cardapio cliente = new Cardapio();
+		cliente.print();
+	}
+	
+	public void fazerPedido()
+	{
+		
+	}
+	
+	public void monitorarPedido()
+	{
+		
+	}
 
 }
