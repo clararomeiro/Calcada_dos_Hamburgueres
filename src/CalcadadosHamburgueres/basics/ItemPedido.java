50765bd480cc4d42432f6ebb58b1ff355c4d3edd
HellenBarros <58638736+HellenBarros@users.noreply.github.com>
2020-10-19 23:21:57 -0300

    Update ItemPedido.java
    Usando Produto

--- src/CalcadadosHamburgueres/basics/ItemPedido.java
+++ src/CalcadadosHamburgueres/basics/ItemPedido.java
@@ -4,38 +4,13 @@ import java.util.Scanner;
 
 
 public class ItemPedido {
-	private EnumHamburguer hamburguer;
-	private EnumBebida bebida;
-	private EnumAcompanhamento acompanhamento;
+	private Produto produto;
 	private int quantidade;
 	private int numeroCliente=0;
 	
-	public ItemPedido(EnumHamburguer hamburguer, EnumBebida bebida, EnumAcompanhamento acompanhamento, int quantidade) {
-		if (hamburguer == null) {
-			this.hamburguer = NumHamburguer.SEMHAMBURGUER; 
-		}
-		
-		else {
-			this.hamburguer = hamburguer;
-		}
-		
-        if (bebida == null) {
-        	this.bebida = NumBebida.SEMBEBIDA;
-        }
-        
-        else{
-        	this.bebida = bebida;
-        }
-        
-        if (acompanhamento == null) {
-        	this.acompanhamento = NumAcompanhamento.SEMACOMPANHAMENTO;
-        }
-        
-        else{
-          this.acompanhamento = acompanhamento;
-        }
-        
-        this.quantidade = quantidade;
+	public ItemPedido(Produto produto) {
+		this.produto = produto;
+        	this.quantidade = quantidade;
 	}
 	public int gerarNumeroCliente(){
 		numeroCliente=numeroCliente+1;
@@ -58,29 +33,8 @@ public class ItemPedido {
 				return observacaoPedido;
 			}
 			
-	}
-	public NumHamburguer getHamburguer() {
-		return hamburguer;
-	}
-
-	public void setHamburguer(NumHamburguer hamburguer) {
-		this.hamburguer=hamburguer;
-	}
-
-	public NumBebida getBebida() {
-		return bebida;
-	}
-
-	public void setBebida(NumBebida bebida) {
-		this.bebida = bebida;
-	}
-
-	public NumAcompanhamento getAcompanhamento() {
-		return acompanhamento;
-	}
-
-	public void setAcompanhamento(NumAcompanhamento acompanhamento) {
-		this.acompanhamento = acompanhamento;
+	public void setProduto(Produto produto) {
+		this.produto=produto;
 	}
 
 	public int getQuantidade() {
