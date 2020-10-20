3e4e4b2d2eaee7468956daa5bd27f2fc60fbb1dd
Ana Clara Romeiro <clararomeirocosta@gmail.com>
2020-10-19 21:25:49 -0300

    Criação classe produto

---
+++ src/CalcadadosHamburgueres/Produto.java
@@ -0,0 +1,10 @@
+package CalcadadosHamburgueres;
+
+import javafx.scene.image.Image;
+
+public class Produto {
+    private String nome;
+    private float preço;
+    private String descrição;
+    private Image imagem;
+}
