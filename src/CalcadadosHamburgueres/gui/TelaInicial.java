22581e3bbd397b554e1cf02d8edb674d4e77c948
Aldenir <aldenirtelless@gmail.com>
2020-10-26 18:18:22 -0300

    TelaInicial update

---
+++ src/CalcadadosHamburgueres/gui/TelaInicial.java
@@ -0,0 +1,39 @@
+package CalcadadosHamburgueres.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TelaInicial extends Application {
+
+	private static Stage stage;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent rood = FXMLLoader.load(getClass().getResource("/TelaInicial.fxml")); // carrega o fxml
+        Scene scene = new Scene(rood); // coloca o fxml em uma cena
+        stage.setTitle("Calçada dos Hamburgueres");
+        stage.setScene(scene); // coloca a cena em uma janela
+        stage.show(); // abre a janela
+        
+        setStage(stage);
+		
+	}
+	
+	public static Stage getStage() {
+		return stage;
+	}
+
+	public static void setStage(Stage stage) {
+		TelaInicial.stage = stage;
+		
+	}
+
+}
--- src/controller/TelaInicialController.java
+++ src/controller/TelaInicialController.java
@@ -3,36 +3,25 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import CalcadadosHamburgueres.basics.Funcionario;
+import beans.Funcionario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class TelaInicialController implements Initializable {
 
-	@FXML
-	private Button btnFuncionario;
-	
-	@FXML 
-	private void botaoFuncionario (ActionEvent event)
-	{
-		
-		Funcionario f = new Funcionario();
-		try {
-			f.start(new Stage());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	
+	@FXML private Button btnFuncionario;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
+		 btnFuncionario.setOnMouseClicked((MouseEvent e)->{
+	            
+	           
+	    });
 		
 	}
 	
