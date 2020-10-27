6ebcf67a60dcf53b87196666b0cad3f0f7071574
Aldenir <aldenirtelless@gmail.com>
2020-10-27 16:38:46 -0300

    Cliente atualizacao

--- src/controller/TelaClienteController.java
+++ src/controller/TelaClienteController.java
@@ -5,15 +5,41 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import telas.TelaAdministrador;
+import telas.TelaCliente;
+import telas.TelaLoginFuncionario;
 
 public class TelaClienteController implements Initializable {
 	  @FXML private TextField txtNome;
 	  @FXML private TextField txtCpf;
+	  @FXML private Button btnEntrar;
+	  
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		// TODO Auto-generated method stub
+		btnEntrar.setOnMouseClicked((MouseEvent e)->{
+			if (txtNome.getText().equals("Nicole") && txtCpf.getText().equals("123456"))
+			{
+				fechar();
+				TelaAdministrador g = new TelaAdministrador();
+				try {
+					g.start(new Stage());
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+				
+		    });
 		
+	}
+	
+	public void fechar ()
+	{
+		TelaCliente.getStage().close();
 	}
 	  
 	  
