ff0d4b9109d4687d7ea3a5169bbbfdab99681ea9
nicole-pereira <nicole-pereira@users.noreply.github.com>
2020-10-27 19:56:10 -0300

    controlador da tela do cardapio visual

--- src/controller/TelaCardapioController.java
+++ src/controller/TelaCardapioController.java
@@ -1,5 +1,43 @@
 package controller;
 
-public class TelaCardapioController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+//import javafx.stage.Stage;
+import telas.TelaCliente;
 
+
+public class TelaCardapioController extends Saida implements Initializable {
+
+    @FXML private Button btnRealizarPedido;
+    @FXML private Button btnVoltar;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        btnVoltar.setOnMouseClicked((MouseEvent e)->{
+            saida();
+        	voltar();
+        });
+        
+        /*btnRealizarPedido.setOnMouseClicked((MouseEvent e)->{
+        	voltar();
+	    	Teladasmeninas a = new Teladasmeninas();
+	    	try {
+				tp.start(new Stage());
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+            
+    });*/
+    
+ }
+    
+    
+    public void voltar(){
+        TelaCliente.getStage().close();
+    }
 }
