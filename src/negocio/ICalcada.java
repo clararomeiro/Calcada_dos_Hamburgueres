f091d0dc4a1dbf858e952d6305f071e6c25d1999
nicole-pereira <nicole-pereira@users.noreply.github.com>
2020-10-29 16:34:30 -0300

    criação da ICalçada

---
+++ src/negocio/ICalcada.java
@@ -0,0 +1,26 @@
+package negocio;
+
+import beans.Cliente;
+import beans.Funcionario;
+//importar a classe nova de pedidos
+import beans.Pagamento;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public interface ICalcada {
+		
+	Cliente logar(String nome, String cpf);
+	
+	public Cliente selecionarCliente(String cpf);
+	
+	void cadastrarPagamento(Pagamento pagamento);
+	
+	List<String> listarFuncionariosString();
+	List<Funcionario> listarFuncionarios();
+	
+	Funcionario logarF(String usuario, String senha);
+	
+	void mudarStatus(String cpf);
+
+}
