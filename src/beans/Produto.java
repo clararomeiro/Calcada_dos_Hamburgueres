ef5e2dc6618cb3932ac9169c3c3d2ea3f16e5efb
Gabriela Maia <maiarodriguesgabriela@gmail.com>
2020-10-31 19:30:17 -0300

    Update Produto.java

--- src/beans/Produto.java
+++ src/beans/Produto.java
@@ -10,7 +10,18 @@ public class Produto implements Serializable {
 	private Bebida bebidas;
 	private Acompanhamento acompanhamentos;
 	
-	//private int quantidade;
+    private int qtdZendaya;
+    private int qtdBBH;
+    private int qtdPS;
+    private int qtdTwice;
+    private int qtdNCT;
+    private int qtdBatata;
+    private int qtdAgua;
+    private int qtdBatataCdd;
+    private int qtdSuco;
+    private int qtdRefri;
+    private int qtdOnion;
+    private int qtdMilkshak;
 	
 	public Produto(Hamburguer hamburgueres, Bebida bebidas, 
 			 Acompanhamento acompanhamentos/*,int quantidade*/) 
@@ -57,10 +68,102 @@ public class Produto implements Serializable {
 
 	public void setAcompanhamentos(Acompanhamento acompanhamentos) {
 		this.acompanhamentos = acompanhamentos;
+	}
+
+	public int getQtdZendaya() {
+		return qtdZendaya;
+	}
+
+	public void setQtdZendaya(int qtdZendaya) {
+		this.qtdZendaya = qtdZendaya;
+	}
+
+	public int getQtdBBH() {
+		return qtdBBH;
+	}
+
+	public void setQtdBBH(int qtdBBH) {
+		this.qtdBBH = qtdBBH;
+	}
+
+	public int getQtdPS() {
+		return qtdPS;
+	}
+
+	public void setQtdPS(int qtdPS) {
+		this.qtdPS = qtdPS;
+	}
+
+	public int getQtdTwice() {
+		return qtdTwice;
+	}
+
+	public void setQtdTwice(int qtdTwice) {
+		this.qtdTwice = qtdTwice;
+	}
+
+	public int getQtdNCT() {
+		return qtdNCT;
+	}
+
+	public void setQtdNCT(int qtdNCT) {
+		this.qtdNCT = qtdNCT;
+	}
+
+	public int getQtdBatata() {
+		return qtdBatata;
+	}
+
+	public void setQtdBatata(int qtdBatata) {
+		this.qtdBatata = qtdBatata;
+	}
+
+	public int getQtdAgua() {
+		return qtdAgua;
+	}
+
+	public void setQtdAgua(int qtdAgua) {
+		this.qtdAgua = qtdAgua;
+	}
+
+	public int getQtdBatataCdd() {
+		return qtdBatataCdd;
+	}
+
+	public void setQtdBatataCdd(int qtdBatataCdd) {
+		this.qtdBatataCdd = qtdBatataCdd;
+	}
+
+	public int getQtdSuco() {
+		return qtdSuco;
+	}
+
+	public void setQtdSuco(int qtdSuco) {
+		this.qtdSuco = qtdSuco;
+	}
+
+	public int getQtdRefri() {
+		return qtdRefri;
+	}
+
+	public void setQtdRefri(int qtdRefri) {
+		this.qtdRefri = qtdRefri;
+	}
+
+	public int getQtdOnion() {
+		return qtdOnion;
+	}
+
+	public void setQtdOnion(int qtdOnion) {
+		this.qtdOnion = qtdOnion;
+	}
+
+	public int getQtdMilkshak() {
+		return qtdMilkshak;
+	}
+
+	public void setQtdMilkshak(int qtdMilkshak) {
+		this.qtdMilkshak = qtdMilkshak;
 	} 
 		
 	}
-
-  
-
-
