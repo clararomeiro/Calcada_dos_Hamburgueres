45f0820b272be667f137b2fd81034a4488549b47
Gabriela Maia <maiarodriguesgabriela@gmail.com>
2020-11-01 18:58:25 -0300

    Update TelaNotaFiscalController.java

--- src/controller/TelaNotaFiscalController.java
+++ src/controller/TelaNotaFiscalController.java
@@ -8,6 +8,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import beans.Pedido;
+import telas.TelaNotaFiscal;
+import negocio.FachadaCalcada;
+import negocio.ICalcada;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -17,14 +21,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import telas.TelaNotaFiscal;
-import telas.TelaPedidos;
-import negocio.FachadaCalcada;
-import negocio.ICalcada;
 
 public class TelaNotaFiscalController extends Saida implements Initializable{
 	@FXML private Button btnSair;
 	@FXML private ListView<String> lvNotaFiscal;
+	
+	ICalcada fachada;
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
@@ -42,7 +44,21 @@ public class TelaNotaFiscalController extends Saida implements Initializable{
 	
 	private void listar() {
 		ArrayList<String> listaNotaFiscal = new ArrayList<>();
-		
+		try {
+            fachada = FachadaCalcada.getInstance();
+        } catch (IOException ex) {
+            Logger.getLogger(TelaPedidosController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(TelaPedidosController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+		for (Pedido p : fachada.listarPedidos()) {
+            
+			listaNotaFiscal.add(p.toString());   
+		}
+		for (Pagamento p : fachada.listarPagamento()) {
+            
+			listaNotaFiscal.add(p.toString());   
+       }
 	}
 	
 	private void fechar(){
