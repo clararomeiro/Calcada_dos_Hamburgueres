c374dc9b442df5de237c16d90608208a66ff1b2e
HellenBarros <hellenbarrosac@gmail.com>
2020-11-01 18:42:51 -0300

    Update RepositorioPagamento.java
    final repositorio

--- src/dados/RepositorioPagamento.java
+++ src/dados/RepositorioPagamento.java
@@ -89,7 +89,77 @@ public class RepositorioPagamento implements IRepositorioPagamento ,Serializable
 		
 	}
 	
+	private static RepositorioPagamento lerArquivo() {
+		RepositorioPagamento instanciaLocal =  null;
+		File f = new File("baseDados" + File.separatorChar+"pedido"+  File.separatorChar+"arqVendas.dat");
+	    FileInputStream fis = null;
+	    ObjectInputStream ois = null;
+	   
+	    try{
+	    	
+	    	fis = new FileInputStream(f);
+	 	    ois = new ObjectInputStream(fis);
+	 	    Object o = ois.readObject();
+	 	    instanciaLocal = (RepositorioPagamento) o;
+	 	    
+	      }
+	      catch(Exception e){
+	    	  
+	        
+	    	  instanciaLocal = new RepositorioPagamento();
+	        
+	      }
+	      finally{
+	        if(ois!=null){
+	            try{
+	              ois.close();
+	            }
+	            catch(IOException e){
+	              System.out.println("N�o foi poss�vel fechar o arquivo!");
+	              e.printStackTrace();
+	            }
+	        }
+	      }
 
+	    return instanciaLocal;
+	}
+	
+
+	@Override
+	public void salvar() {
+		if(instance == null){
+			return;
+		}
+		     
+		File f = new File("baseDados" + File.separatorChar+"pedido"+  File.separatorChar+"arqVendas.dat");
+	    FileOutputStream fos = null;
+	    ObjectOutputStream oos = null;
+	    try{
+	    	 if(!f.exists()) {
+	    		 f.createNewFile();
+	    	 }
+	    	 fos = new FileOutputStream(f);
+			 oos = new ObjectOutputStream(fos);
+			 
+			 oos.writeObject(instance);   
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }finally{
+	        if(oos!=null){
+	          
+	          try{
+	              oos.close();
+	          }catch(IOException e){
+	        	  System.out.println("Nao foi possivel fechar o arquivo.");
+	        	  e.printStackTrace();
+	          }
+	        }
+	    }
+	}
+
+	
+
+	
 	
 	
 	
