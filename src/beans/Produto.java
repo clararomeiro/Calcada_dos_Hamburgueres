0c37df00d776620ee8b3e135542b3fc20a0159ee
Ana Clara Romeiro <57761162+clararomeiro@users.noreply.github.com>
2020-11-02 03:36:00 -0300

    Update Produto.java

--- src/beans/Produto.java
+++ src/beans/Produto.java
@@ -179,15 +179,6 @@ public class Produto implements Serializable {
 	public void setQtdMilkshak(int qtdMilkshak) {
 		this.qtdMilkshake = qtdMilkshak;
 	}
-
-	public double getPrecoFinal(){
-		double preco = qtdAgua*Bebida.AGUA.getValor()+ qtdSuco*Bebida.SUCO.getValor() + qtdRefri*Bebida.REFRIGERANTE.getValor()
-				+ qtdBatata*Acompanhamento.BATATA_FRITA.getValor() + qtdBatataCdd*Acompanhamento.BATATA_CHEDDAR.getValor()
-				+ qtdOnion+Acompanhamento.ONION_RINGS.getValor() + qtdMilkshake*Acompanhamento.MILK_SHAKE.getValor()
-				+ qtdBBH*Hamburguer.BBH.getValor() + qtdZendaya*Hamburguer.ZENDAYA.getValor()
-				+ qtdTwice*Hamburguer.TWICE.getValor() + qtdNCT*Hamburguer.NCT.getValor()
-				+ qtdPS*Hamburguer.PRISCILA_SENNA.getValor();
-		return preco;
-	}
+	
 		
 	}
